9d07e183c7aff7e803df61df96398019099d1e62
DerBaumann <natur5210@gmail.com>
2026-06-05 13:58:23 +0200

    Add form fields to login form

--- src/app/auth/pages/login/login.ts
+++ src/app/auth/pages/login/login.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  username = new FormControl('');
+  password = new FormControl('');
+}
