d91802d15be0e543944786df65eed7eed4e59e6c
DerBaumann <natur5210@gmail.com>
2026-06-05 10:35:38 +0200

    Setup angular router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Home } from './core/pages/home/home';
+import { NotFound } from './core/pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home, title: 'Home' },
+  { path: '**', component: NotFound, title: '404 Page not found!' },
+];
