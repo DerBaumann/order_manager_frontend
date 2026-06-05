60b1a5afa05d19b432ca5284e94b2f72a1303d84
DerBaumann <natur5210@gmail.com>
2026-06-05 13:36:05 +0200

    Add auth package. Add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Home } from './core/pages/home/home';
 import { NotFound } from './core/pages/not-found/not-found';
 import { OrderList } from './orders/pages/order-list/order-list';
 import { ContactList } from './contacts/pages/contact-list/contact-list';
+import { Login } from './auth/pages/login/login';
 
 export const routes: Routes = [
   { path: '', component: Home, title: 'Home' },
+  { path: 'login', component: Login, title: 'Login' },
   { path: 'orders', component: OrderList, title: 'Orders' },
   { path: 'contacts', component: ContactList, title: 'Contacts' },
   { path: '**', component: NotFound, title: '404 Page not found!' },
