bc7a2db68497cd914094037af2786e4ab95d8608
DerBaumann <natur5210@gmail.com>
2026-06-05 11:36:56 +0200

    Add links to sidenav

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatSidenavModule],
+  imports: [RouterOutlet, RouterLink, MatSidenavModule, MatButtonModule, MatListModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
