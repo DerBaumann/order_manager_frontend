67940f2e85aeff53269949f0097f146b65694408
DerBaumann <natur5210@gmail.com>
2026-06-11 08:16:06 +0200

    Add linting. Add ContactService .store, .update and .delete

--- src/app/contacts/services/contact-service.ts
+++ src/app/contacts/services/contact-service.ts
@@ -1,36 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { map, Observable } from 'rxjs';
-import { Contact } from '../models/contact';
+import { Contact, CreateContact } from '../models/contact';
 
 @Service()
 export class ContactService {
   private readonly baseUrl = 'http://localhost:9090/api/contacts';
   private http = inject(HttpClient);
 
+  // @RolesAllowed({Roles.Read, Roles.Update, Roles.Admin})
   getAll(): Observable<Contact[]> {
     return this.http.get(this.baseUrl).pipe(map((c) => Contact.array().parse(c)));
   }
 
-  // @GetMapping("/{id}")
   // @RolesAllowed({Roles.Read, Roles.Update, Roles.Admin})
   getByID(id: number): Observable<Contact> {
     return this.http.get(`${this.baseUrl}/${id}`).pipe(map((c) => Contact.parse(c)));
   }
-  //
-  // @PostMapping("/")
+
   // @RolesAllowed({Roles.Update, Roles.Admin})
-  // public ResponseEntity<Contact> store(@RequestBody @Valid ContactRequestDTO requestDTO) {
-  //     return new ResponseEntity<>(contactService.create(requestDTO), HttpStatus.CREATED);
-  // }
-  //
-  // @PutMapping("/{id}")
+  store(createContact: CreateContact): Observable<Contact> {
+    return this.http.post(this.baseUrl, createContact).pipe(map((c) => Contact.parse(c)));
+  }
+
   // @RolesAllowed({Roles.Update, Roles.Admin})
-  // public @ResponseBody Contact update(@RequestBody @Valid ContactRequestDTO requestDTO, @PathVariable Long id) {
-  //     return contactService.update(id, requestDTO);
-  // }
-  //
-  // @DeleteMapping("/{id}")
+  update(createContact: CreateContact, id: number): Observable<Contact> {
+    return this.http.put(`${this.baseUrl}/${id}`, createContact).pipe(map((c) => Contact.parse(c)));
+  }
+
   // @RolesAllowed({Roles.Admin})
-  // public @ResponseBody Contact destroy(@PathVariable Long id) {
+  delete(id: number): Observable<Contact> {
+    return this.http.delete(`${this.baseUrl}/${id}`).pipe(map((c) => Contact.parse(c)));
+  }
 }
