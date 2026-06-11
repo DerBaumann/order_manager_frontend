f40ec0cc02e27b8515b68e513377e09b94987b4d
DerBaumann <natur5210@gmail.com>
2026-06-11 08:19:31 +0200

    Change ContactService methods to lambda because they all immediatly return

--- src/app/contacts/services/contact-service.ts
+++ src/app/contacts/services/contact-service.ts
@@ -9,27 +9,22 @@ export class ContactService {
   private http = inject(HttpClient);
 
   // @RolesAllowed({Roles.Read, Roles.Update, Roles.Admin})
-  getAll(): Observable<Contact[]> {
-    return this.http.get(this.baseUrl).pipe(map((c) => Contact.array().parse(c)));
-  }
+  getAll = (): Observable<Contact[]> =>
+    this.http.get(this.baseUrl).pipe(map((c) => Contact.array().parse(c)));
 
   // @RolesAllowed({Roles.Read, Roles.Update, Roles.Admin})
-  getByID(id: number): Observable<Contact> {
-    return this.http.get(`${this.baseUrl}/${id}`).pipe(map((c) => Contact.parse(c)));
-  }
+  getByID = (id: number): Observable<Contact> =>
+    this.http.get(`${this.baseUrl}/${id}`).pipe(map((c) => Contact.parse(c)));
 
   // @RolesAllowed({Roles.Update, Roles.Admin})
-  store(createContact: CreateContact): Observable<Contact> {
-    return this.http.post(this.baseUrl, createContact).pipe(map((c) => Contact.parse(c)));
-  }
+  store = (createContact: CreateContact): Observable<Contact> =>
+    this.http.post(this.baseUrl, createContact).pipe(map((c) => Contact.parse(c)));
 
   // @RolesAllowed({Roles.Update, Roles.Admin})
-  update(createContact: CreateContact, id: number): Observable<Contact> {
-    return this.http.put(`${this.baseUrl}/${id}`, createContact).pipe(map((c) => Contact.parse(c)));
-  }
+  update = (createContact: CreateContact, id: number): Observable<Contact> =>
+    this.http.put(`${this.baseUrl}/${id}`, createContact).pipe(map((c) => Contact.parse(c)));
 
   // @RolesAllowed({Roles.Admin})
-  delete(id: number): Observable<Contact> {
-    return this.http.delete(`${this.baseUrl}/${id}`).pipe(map((c) => Contact.parse(c)));
-  }
+  delete = (id: number): Observable<Contact> =>
+    this.http.delete(`${this.baseUrl}/${id}`).pipe(map((c) => Contact.parse(c)));
 }
