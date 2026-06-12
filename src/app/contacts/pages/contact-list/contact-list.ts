676b9ee367bbfbf9fe7adcfa60cda73577e7c948
DerBaumann <natur5210@gmail.com>
2026-06-12 08:29:19 +0200

    Add Edit and Delete actions to contact table

--- src/app/contacts/pages/contact-list/contact-list.ts
+++ src/app/contacts/pages/contact-list/contact-list.ts
@@ -4,17 +4,33 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { ContactTableRow, tableRowFromContact } from '../../models/contact';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { IsInRolesDirective } from '../../../auth/directives/app-is-in-roles.dir';
+import { AppRoles } from '../../../app.roles';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-contact-list',
-  imports: [MatTableModule, MatSortModule, MatButtonModule, RouterLink],
+  imports: [
+    MatIconModule,
+    MatTableModule,
+    MatSortModule,
+    MatButtonModule,
+    RouterLink,
+    IsInRolesDirective,
+  ],
   templateUrl: './contact-list.html',
   styleUrl: './contact-list.scss',
 })
 export class ContactList implements OnInit {
   private readonly service = inject(ContactService);
+  private readonly router = inject(Router);
+  private readonly dialog = inject(MatDialog);
+
   @ViewChild(MatSort) sort!: MatSort;
+
+  protected readonly roles = AppRoles;
 
   displayedColumns: string[] = [
     'id',
@@ -26,6 +42,7 @@ export class ContactList implements OnInit {
     'placeName',
     'postCode',
     'canton',
+    'actions',
   ];
   dataSource = new MatTableDataSource<ContactTableRow>();
 
@@ -35,4 +52,12 @@ export class ContactList implements OnInit {
 
   fetchData = () =>
     this.service.getAll().subscribe((c) => (this.dataSource.data = c.map(tableRowFromContact)));
+
+  // TODO: Add page
+  edit = (id: number) => this.router.navigate(['contacts', 'edit', id]);
+
+  // TODO: Implement delete
+  delete(id: number) {
+    console.log('TODO');
+  }
 }
